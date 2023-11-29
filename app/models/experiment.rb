c028e5d65b9ce659dd9a12c22798bd7f0c5e4dee
Rodrigo Vieira Gusmao <105116345+RodrigoVGusmao@users.noreply.github.com>
2023-11-29 19:08:41 -0300

    Update experiment.rb

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,17 +1,3 @@
-class trial_result
-    attr_reader :status;
-    attr_reader :position_x;
-    attr_reader :position_y;
-    attr_reader :battery;
-
-    def get_data(data) #TrialExecution object
-        @position_x = data.log.scan("'x':\s*(.),").last;
-        @position_y = data.log.scan("'y':\s*(.),").last;
-        @battery = data.log.scan("'bettery-level':\s*'(.)',").last;
-        @status = data.status;
-    end
-end
-
 class Experiment < ApplicationRecord
     has_many :trials
 end
