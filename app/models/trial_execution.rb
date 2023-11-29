9f18e181853ab1f0d345b9803f97dab4c50d25cc
Rodrigo Vieira Gusmao <105116345+RodrigoVGusmao@users.noreply.github.com>
2023-11-29 19:25:26 -0300

    Movido e modificado classe trial_result para trial_execution.rb, classe é responsável por tratar os resultados dos logs do experimento.
    Movido e modificado classe trial_result para trial_execution.rb, classe é responsável por tratar os resultados dos logs do experimento.
    Problemas da classe se mantém:
     -É preciso que seja confirmado se a entrada dos logs vão ser informados para o card durante a execução do experimento.
     -É preciso confirmar se dados do campo log do Banco de Dados estão sanitizados, e se é seguro usar com regex, sob risco de apresentar falha de visualização e possivel ataque de injeção.

--- app/models/trial_execution.rb
+++ app/models/trial_execution.rb
@@ -1,3 +1,17 @@
+class trial_result
+    attr_reader :status;
+    attr_reader :position_x;
+    attr_reader :position_y;
+    attr_reader :battery;
+
+    def initialize(data) #TrialExecution object
+        @position_x = data.log.scan("'x':\s*(.),").last;
+        @position_y = data.log.scan("'y':\s*(.),").last;
+        @battery = data.log.scan("'bettery-level':\s*'(.)',").last;
+        @status = data.status;
+    end
+end
+
 class TrialExecution < ApplicationRecord
     belongs_to :trial
 end
