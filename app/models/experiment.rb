c1244db40c59ed5283eb29b9fd338297211af110
Rodrigo Vieira Gusmao <105116345+RodrigoVGusmao@users.noreply.github.com>
2023-11-29 18:36:51 -0300

    Update experiment.rb
    Adicionado classe de dados usado nos cards, não está completo a questão de relacionar a entrada e o experimento, e nem está certo sobre quais informações devem ser pegas.
    Existem possiveis bugs relacionados ao regex, pode ocorrer de string der match em algo inesperado (confirmar quais caracteres podem ser usados no nome do robô), ou que log tenha um formado customizado ou inesperado (confirmar se arquivo log no db não pode ser inserido de forma irregular sob risco de falha na visualização da informação ou injection de código malicioso)

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,3 +1,17 @@
+class trial_result
+    attr_reader :strStatus;
+    attr_reader :fpPositionX;
+    attr_reader :fpPositionY;
+    attr_reader :fpBattery;
+
+    def getData()
+        @fpPositionX = TrialExecution.log.scan("'x':\s*(.),").last;
+        @fpPositionY = TrialExecution.log.scan("'y':\s*(.),").last;
+        @fpBattery = TrialExecution.log.scan("'bettery-level':\s*'(.)',").last;
+        @strStatus = TrialExecution.status;
+    end
+end
+
 class Experiment < ApplicationRecord
     has_many :trials
 end
