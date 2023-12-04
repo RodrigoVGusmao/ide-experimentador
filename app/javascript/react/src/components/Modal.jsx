fcfdc8d13cc55d5b909d098067779e995f0970ae
Maria Claudia <mariaclaudia8.0@hotmail.com>
2023-12-03 23:43:23 -0300

    implementa paginas do modal

--- app/javascript/react/src/components/Modal.jsx
+++ app/javascript/react/src/components/Modal.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "../../../../assets/stylesheets/modal.css";
 
+import BatteryPage from "../components/ModalPages/BatteryPage";
+import LocationPage from "../components/ModalPages/LocationPage";
+import SpeedPage from "../components/ModalPages/SpeedPage";
+import SkillsPage from "../components/ModalPages/SkillsPage";
+
 export default function Modal({ isModalOpen, closeModal }) {
+  const [isBatteryPageOpen, setBatteryPageOpen] = useState(true);
+  const [isLocationPageOpen, setLocationPageOpen] = useState(false);
+  const [isSpeedPageOpen, setSpeedPageOpen] = useState(false);
+  const [isSkillsPageOpen, setSkillsPageOpen] = useState(false);
+
   return (
     <div>
       {isModalOpen && (
@@ -15,26 +26,60 @@ export default function Modal({ isModalOpen, closeModal }) {
                 onClick={closeModal}
               />
             </button>
-            <button onClick={() => handleButtonClick("battery")}>
+            <button
+              onClick={() => {
+                setBatteryPageOpen(true);
+                setLocationPageOpen(false);
+                setSpeedPageOpen(false);
+                setSkillsPageOpen(false);
+              }}
+            >
               <img
                 className="battery"
                 src={"/modal_battery.svg"}
                 alt="Battery"
               />
             </button>
-            <button onClick={() => handleButtonClick("location")}>
+            <button
+              onClick={() => {
+                setBatteryPageOpen(false);
+                setLocationPageOpen(true);
+                setSpeedPageOpen(false);
+                setSkillsPageOpen(false);
+              }}
+            >
               <img
                 className="location"
                 src={"/modal_location.svg"}
                 alt="Location"
               />
             </button>
-            <button onClick={() => handleButtonClick("speed")}>
+            <button
+              onClick={() => {
+                setBatteryPageOpen(false);
+                setLocationPageOpen(false);
+                setSpeedPageOpen(true);
+                setSkillsPageOpen(false);
+              }}
+            >
               <img className="speed" src={"/modal_speed.svg"} alt="Speed" />
             </button>
-            <button onClick={() => handleButtonClick("skills")}>
+            <button
+              onClick={() => {
+                setBatteryPageOpen(false);
+                setLocationPageOpen(false);
+                setSpeedPageOpen(false);
+                setSkillsPageOpen(true);
+              }}
+            >
               <img className="skills" src={"/modal_skills.svg"} alt="Skills" />
             </button>
+            <div className="modal-container">
+              {isBatteryPageOpen && <BatteryPage />}
+              {isLocationPageOpen && <LocationPage />}
+              {isSpeedPageOpen && <SpeedPage />}
+              {isSkillsPageOpen && <SkillsPage />}
+            </div>
           </div>
         </div>
       )}
