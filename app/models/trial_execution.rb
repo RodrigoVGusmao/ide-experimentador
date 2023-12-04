5c3b8916a2eb9d90795a83888c683f5ba5ab2f46
Rodrigo Vieira Gusmao <105116345+RodrigoVGusmao@users.noreply.github.com>
2023-12-03 22:26:13 -0300

    Adicionado valor padrão para campos da classe TrialResult em trial_execution.rb
    Adicionado valor padrão pro caso de não ser encontrado dados nos logs de resultado do experimento

--- app/models/trial_execution.rb
+++ app/models/trial_execution.rb
@@ -5,10 +5,10 @@ class TrialResult
     attr_reader :battery;
 
     def initialize(data) #TrialExecution object
-        @position_x = data.log.match(".*'x':\s*(.+?)(,|})")[1];
-        @position_y = data.log.match(".*'y':\s*(.+?)(,|})")[1];
-        @battery = data.log.match(".*'battery-level':\s*'(.+?)'(,|})")[1];
-        @status = data.status;
+        @position_x = data.log.match(".*'x':\s*(.+?)(,|})") != nil ? data.log.match(".*'x':\s*(.+?)(,|})")[1] : "-";
+        @position_y = data.log.match(".*'y':\s*(.+?)(,|})") != nil ? data.log.match(".*'y':\s*(.+?)(,|})")[1] : "-";
+        @battery = data.log.match(".*'battery-level':\s*'(.+?)'(,|})") != nil ? data.log.match(".*'battery-level':\s*'(.+?)'(,|})")[1] : "-";
+        @status = (data.status == nil or data.status.match("^\s*$")) ? "N/A" : data.status;
     end
 end
 
