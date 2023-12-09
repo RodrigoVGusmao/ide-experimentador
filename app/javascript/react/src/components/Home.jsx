70903a801631c8ab890f7c992f38b0d459c51317
Maria Claudia <mariaclaudia8.0@hotmail.com>
2023-12-08 21:47:35 -0300

    limpa codigo

--- app/javascript/react/src/components/Home.jsx
+++ app/javascript/react/src/components/Home.jsx
@@ -1,29 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ExperimentPage from "./ExperimentPage";
 
 const Home = (props) => {
-  // let [count, setCount] = useState(0);
-  // let [req, setReq] = useState("");
-  // useEffect(() => {
-  //   fetch("/hello/worldGet")
-  //     .then(async (res) => {
-  //       const parse = await res.text();
-  //       console.log(parse);
-  //       return parse;
-  //     })
-  //     .then((res) => setReq(res));
-  // }, []);
-
-  const [isModalOpen, setModalOpen] = useState(false);
-
-  function openModal() {
-    setModalOpen(true);
-  }
-
-  function closeModal() {
-    setModalOpen(false);
-  }
-
   return (
     <div>
       <ExperimentPage />
