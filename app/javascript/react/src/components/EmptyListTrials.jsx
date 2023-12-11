f50db4ec935f582bdc58de8429d8fb254a78f5a7
Maria Claudia <mariaclaudia8.0@hotmail.com>
2023-12-11 19:59:38 -0300

    implementa caso de experimento sem trials

---
+++ app/javascript/react/src/components/EmptyListTrials.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../../../assets/stylesheets/experimentPage.css";
+
+export default function EmptyListTrials() {
+  return (
+    <div className="emptyListTrials">
+      <p>Não há testes para serem feitos.</p>
+    </div>
+  );
+}
