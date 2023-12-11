73033592ac2f154a7dc14ce26a7279946ff26413
Maria Claudia <mariaclaudia8.0@hotmail.com>
2023-12-11 15:37:26 -0300

    limpa codigo

--- app/javascript/react/src/components/Card.jsx
+++ app/javascript/react/src/components/Card.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Modal from "./Modal";
 import "../../../../assets/stylesheets/card.css";
 import "./ToggleCard.js";
 import DropdownCard from "./DropdownCard.jsx";
